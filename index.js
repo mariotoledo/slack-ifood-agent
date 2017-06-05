2bfd2458efba45716e7c5b07ac33013eca4c58c7
Mario Toledo <mtoledo@2mundos.net>
2017-06-05 16:54:38 -0300

    removing token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var slackTerminal = require('slack-terminalize');
 
-slackTerminal.init('xoxb-187134790194-VOwZshMN1qT4ObQvkFTppTRv', {
+slackTerminal.init('', {
 }, {
 	CONFIG_DIR: __dirname + '/config',
 	COMMAND_DIR: __dirname + '/commands'
